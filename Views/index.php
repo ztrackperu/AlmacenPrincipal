dc69e48d01b12000f8da72fe262abc49ba0461e8
ztrackperu <ztrack@zgroup.com.pe>
2025-03-10 12:29:56 -0500

    base principal 2

--- Views/index.php
+++ Views/index.php
@@ -18,7 +18,7 @@
     </section>
     <section class="login-content">
         <div class="logo">
-            <h1>ALMACEN PRINCIPAL</h1>
+            <h3>ALMACEN PRINCIPAL</h3>
         </div>
         <div class="login-box">
             <form class="login-form" id="frmLogin" onsubmit="frmLogin(event);">
